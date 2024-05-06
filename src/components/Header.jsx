e83e789b590acea01cd762f8f79a7c515c915b07
Bhuro11 <bhargavtalpada2004@gmail.com>
2024-05-07 01:52:38 +0530

    search

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,33 @@
 import { FaBars, FaUser } from "react-icons/fa";
 import { IoIosSearch } from "react-icons/io";
-import { YOUTUBE_LOGO_URL } from "../utils/constants";
+import { YOUTUBE_LOGO_URL, YOUTUBE_SEARCH_API } from "../utils/constants";
 import { useDispatch } from "react-redux";
 import { toggleMenu } from "../redux/appSlice";
 import { IoMdNotificationsOutline } from "react-icons/io";
 import { BiVideoPlus } from "react-icons/bi";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+
+  const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+
+    const timer = setTimeout(() => {
+      getSearchSuggestions();
+    }, 2000);
+
+    return () => {
+      clearTimeout(timer);
+    }
+
+  }, [searchQuery]);
+
+  const getSearchSuggestions = async () => {
+    console.log("api call", searchQuery);
+    const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+    const json = await data.json();
+  };
 
   const dispatch = useDispatch();
 
@@ -23,7 +44,7 @@ const Header = () => {
             </a>
         </div>
         <div className=" flex justify-center items-center placeholder-black text-black text-xl">
-            <input type="text" placeholder="Search" className="w-[500px] border border-black py-[2px] placeholder:text-[16px] px-5 rounded-l-full" />
+            <input type="text" value={searchQuery} onChange={((e) => setSearchQuery(e.target.value))}  placeholder="Search" className="w-[500px] border border-black py-[2px] placeholder:text-[16px] px-5 rounded-l-full" />
             <button className="py-[5px] px-4 bg-gray-400 text-white rounded-r-full" ><IoIosSearch className="text-2xl"  /></button>
         </div>
         <div className="flex items-center gap-3 mr-3">
