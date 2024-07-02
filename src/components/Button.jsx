c4aaef494eba338bd06cb5e0b461627509cacfef
Bhuro11 <bhargavtalpada2004@gmail.com>
2024-07-02 18:59:19 +0530

    buttonos

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 
 const Button = ({name}) => {
   return (
-    <div className="">
+    <div className="relative">
         <button className=" mx-1 px-3 rounded-md bg-zinc-400 text-white text-lg">{name}</button>
     </div>
   )
--- src/components/ButtonList.jsx
+++ src/components/ButtonList.jsx
@@ -1,12 +1,13 @@
 import Button from "./Button";
+import {BsArrowLeftCircleFill, BsArrowRightCircleFill} from 'react-icons/bs';
 
 export const ButtonList = () => {
 
-  const nameList = ["All","Live", "Music", "Gaming", "Cricket",  "Trailer", "JavaScript", "ReactJS", "WebDevelopment", "SamsungGalaxy", "ViratKohli", "TarakMehta", "KapilSharma", "Watched", "Comedies"]; 
+  const nameList = ["All","Live", "Music", "Gaming", "Cricket",  "Trailer", "JavaScript", "ReactJS", "WebDevelopment", "SamsungGalaxy", "ViratKohli", "TarakMehta", "KapilSharma", "Watched"]; 
 
   return (
-    <div className="mt-14 h-10 bg-white w-screen overflow-x-scroll">
-      <div className=" flex ml-4 h-10 bg-white w-screen">
+    <div className=" mt-14 h-10 bg-white w-screen overflow-x-scroll">
+      <div className=" flex ml-4 h-10 w-screen">
         {
           nameList.map((buttonName) => 
             <Button key={buttonName} name={buttonName} />
